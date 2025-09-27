3a1be0b0bd82577d86ac1e1339982005465edeea
Nay Macbook <nayookyaw.nok@gmail.com>
2025-09-27 22:05:44 +1200

    setting page, retreive google api key

--- frontend-reactjs/src/pages/Settings.tsx
+++ frontend-reactjs/src/pages/Settings.tsx
@@ -3,19 +3,32 @@ import { getGoogleMapsApiKeyApi } from '../api/http';
 import MaskedText from '../components/MaskedText'; 
 
 export default function Settings(){ 
-  const [key,setKey]=React.useState(''); 
+  const [googleApiKey,setGoogleApiKey]=React.useState(''); 
   const [loading,setLoading]=React.useState(true); 
   
   React.useEffect(()=>{ 
-    // getGoogleMapsApiKeyApi().then(setKey).finally(()=>setLoading(false)); 
-  },[]); 
+    getGoogleMapsApiKey();
+  },[]);
+
+  const getGoogleMapsApiKey = () => {
+    console.log ('Fetching Google Maps API key from backend...');
+    getGoogleMapsApiKeyApi()
+      .then((res: any) => {
+        setGoogleApiKey(res?.data?.apiKey || "-");
+        setLoading(false);
+      })
+      .catch((e: any) => {
+        console.error("Failed to load Google Maps API key", e);
+        setGoogleApiKey("");
+      });
+  }
   
   return (
     <div className="page">
       <h1>Settings</h1>
       <div className="card">
         <h3>Google Maps API Key</h3>
-        {loading?'Loading…': key ? <MaskedText value={key}/> : <em>Not configured</em>}
+        {loading?'Loading…': googleApiKey ? <MaskedText value={googleApiKey}/> : <em>Not configured</em>}
         <p style={{marginTop:12}}>Retrieved from backend (Prisma/AppConfig).</p>
       </div>
     </div>
